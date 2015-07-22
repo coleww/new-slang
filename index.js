53e830ac175ceed4bd7698b86c4a9200aa84589c
Cole Willsea <coleww@gmail.com>
2015-07-22 08:04:52 -0700

    pass a map to convert the text

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var conversions = {
+var conversionMap = {
+  ough: 'ewe',
+  ant: 'aunt',
+  end: 'ihnd',
+  and: 'uhn',
+  ent: 'ind',
+  ast: 'inst',
   ou: 'oo',
-  i: 'eye',
-  e: 'ee',
   ow: 'au',
+  il: 'iliil',
+  ag: 'ack'
 }
 
-module.exports = function(text, lev){
+module.exports = function(text, lev, conv){
   var newStr = text
   var level = lev !== undefined ? lev : 0.5
+  var conversions = conv !== undefined ? conv : conversionMap
   Object.keys(conversions).forEach(function(substr){
     if(newStr.indexOf(substr) !== -1 && Math.random() < level){
       newStr = newStr.replace(new RegExp(substr, 'g'), conversions[substr])
--- test.js
+++ test.js
@@ -4,5 +4,16 @@ var newSlang = require('./')
 
 tap.test('slangifies',function(t){
   t.plan(1)
-  t.equal(newSlang('you will be magic', 1), 'yoo weeyeell bee mageeyeec')
+  t.equal(newSlang('through you we will be magic', 1), 'threwe yoo we wiliill be mackic')
+})
+
+tap.test('conversion map can be passed',function(t){
+  t.plan(1)
+  t.equal(newSlang('tofu', 1, {to: 'burr', fu: 'ito'}), 'burrito')
+})
+
+
+tap.test('level is optional',function(t){
+  t.plan(1)
+  t.equal(typeof newSlang('pasta sauce and gravy'), 'string')
 })
